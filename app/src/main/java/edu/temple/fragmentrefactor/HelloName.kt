21928a63e5723c6a8e842408809d3b551f637620
Niaz Baharudeen <tuk05348@temple.edu>
2024-03-20 15:35:21 -0400

    Created a new blank fragment with a corresponding layout file.

---
+++ app/src/main/java/edu/temple/fragmentrefactor/HelloName.kt
@@ -0,0 +1,21 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HelloName : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hello_name, container, false)
+    }
+}
