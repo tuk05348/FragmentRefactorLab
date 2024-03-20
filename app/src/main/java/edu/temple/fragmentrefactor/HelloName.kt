4a210598253e86f038614db1b927a46d0c0fa432
Niaz Baharudeen <tuk05348@temple.edu>
2024-03-20 15:45:48 -0400

    Added the button onclick listener and display text code to the fragment.

--- app/src/main/java/edu/temple/fragmentrefactor/HelloName.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/HelloName.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class HelloName : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -16,6 +20,25 @@ class HelloName : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hello_name, container, false)
+
+        return inflater.inflate(R.layout.fragment_hello_name, container, false).apply {
+            val displayTextView = findViewById<TextView>(R.id.displayTextView)
+            val nameEditText = findViewById<EditText>(R.id.nameEditText)
+            val changeButton = findViewById<Button>(R.id.changeButton)
+
+            changeButton.setOnClickListener {
+                val name = nameEditText.text
+
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
